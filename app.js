f3677d535f4ad3a0b2536a60c84703fd921cbdb4
eugene220117 <eugene220117@gmail.com>
2022-03-02 11:32:00 +0200

    second with conditions

--- app.js
+++ app.js
@@ -8,12 +8,35 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies.a = b;
-personalMovieDB.movies.b = d;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено мало");
+  } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Error");
+  }
+}
 
 console.log(personalMovieDB);
+
+function calc(a, b) {
+  return a + b;
+}
+
+console.log(calc(4, 5));
+console.log(calc(1, 2));
+console.log(calc(5, 3));
